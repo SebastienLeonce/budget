5a5fac25317a1cbe064f054bf1100843e22574f8
Seb <sebastienleonce91@gmail.com>
2021-08-24 17:45:38 +0200

    add gulp

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,7 +34,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
 });
 
 module.exports = app;
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+var spawn = require('child_process').spawn;
+var node;
+var vue;
+
+
+function server(cb) {
+    if (node) node.kill()
+    node = spawn('node', ['./bin/www'], {
+        stdio: 'inherit'
+    })
+    node.on('close', function (code) {
+        if (code === 8) {
+            gulp.log('Error detected, waiting for changes...');
+        }
+    });
+
+    cb()
+}
+
+function front(cb) {
+    if (vue) vue.kill()
+    vue = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['run', 'build:dev'], {
+        stdio: 'inherit'
+    });
+
+    vue.on('close', function (code) {
+        if (code === 8) {
+            gulp.log('Error detected, waiting for changes...');
+        }
+        cb()
+    });
+}
+
+function reload(cb) {
+    browserSync.reload();
+    cb();
+}
+
+
+function defaultTask(cb) {
+    browserSync.init({
+        proxy: "localhost:3000",
+        port: 5000,
+    });
+
+    gulp.watch(["app.js", "./bin/www", "./routes/*.js"], server);
+    gulp.watch(["./src/**/*.js", "./src/**/*.vue"], gulp.series(front, reload));
+    
+
+    cb();
+}
+
+process.on('exit', function () {
+    if (node) node.kill()
+})
+
+exports.default = gulp.series(server, defaultTask)
