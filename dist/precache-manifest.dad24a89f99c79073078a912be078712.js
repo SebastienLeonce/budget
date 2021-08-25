491d0af51a3eae057ff0aa256758c1a66e80db32
Seb <sebastienleonce91@gmail.com>
2021-08-25 11:29:36 +0200

    build prod

---
+++ dist/precache-manifest.dad24a89f99c79073078a912be078712.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602d45decadc78f7c276",
+    "url": "/css/app.5296ce8e.css"
+  },
+  {
+    "revision": "527123ed3215aa6bddfb",
+    "url": "/css/chunk-vendors.68a93634.css"
+  },
+  {
+    "revision": "028c7b0fbdc1d5b38c7be5311fe7f0a3",
+    "url": "/fonts/material-icons-outlined.028c7b0f.woff2"
+  },
+  {
+    "revision": "9d80a4209670d7a9be52df58c409c1f3",
+    "url": "/fonts/material-icons-round.9d80a420.woff2"
+  },
+  {
+    "revision": "acfc02cc3fbbccd242f3274665b0bb8f",
+    "url": "/fonts/material-icons-sharp.acfc02cc.woff2"
+  },
+  {
+    "revision": "8208136fb8d2e7da8da3aee13f1caf2a",
+    "url": "/fonts/material-icons.8208136f.woff"
+  },
+  {
+    "revision": "a7de13342a670c0265a2f9b0df431be4",
+    "url": "/fonts/material-icons.a7de1334.woff2"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2a263eaee16f200781a38f5170bc5de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "602d45decadc78f7c276",
+    "url": "/js/app.fedb066f.js"
+  },
+  {
+    "revision": "527123ed3215aa6bddfb",
+    "url": "/js/chunk-vendors.fd80454c.js"
+  },
+  {
+    "revision": "a2c59eed892b476901707d6302c3a60a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
